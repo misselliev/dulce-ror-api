62006a13a94ad67bd5aadbdd31eca4216fe87157
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-27 13:53:31 -0700

    Adding user migration to db

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend Devise::Models
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
