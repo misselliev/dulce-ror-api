07451509c1f56683ae0d0bbd450ff1b4793f3bf0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-31 19:20:34 -0700

    Updating schedules controller with show, create and destroy actions

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -5,6 +5,31 @@ module Api
         @schedules = Schedule.all
         render json: @schedules
       end
+
+      def show
+        # @schedules = User.find(params[:id]).schedules
+        @schedules = Schedule.find_by_id(params[:id])
+        render json: @schedules
+      end
+
+      def create
+        @schedule = Schedule.create(schedule_params)
+        if @schedule.valid?
+          render json: @schedule
+        else
+          render json: { errors: @schedule.errors.full_messages }
+        end
+      end
+
+      def destroy
+        @schedule = Schedule.find_by(talk_id: params[:id])
+        @schedule.destroy
+      end
+
+      private
+      def schedule_params
+        params.permit(:user_id, :talk_id)
+      end
     end
   end
 end
