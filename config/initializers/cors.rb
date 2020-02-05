2f72160cd8a87126aed37771fc455b23397d2a7e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-04 19:37:40 -0700

    Updating CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'https://intense-garden-00658.herokuapp.com'
 
     resource '*',
       headers: :any,
