80394f4e42eede9a8960f6cfeec92fdf89262ca8
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-06 12:28:21 -0700

    Adding presence validations

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,6 +2,8 @@ class Talk < ApplicationRecord
   before_save :capitalize_fields
   belongs_to :creator, :foreign_key => "creator_id", :class_name => "User"
   has_many :schedules, dependent: :destroy
+  
+  validates_presence_of :title, :description, :location, :date, :time, :speaker_name, :speaker_title, :creator_id
 
    def capitalize_fields
       self.title.capitalize!
