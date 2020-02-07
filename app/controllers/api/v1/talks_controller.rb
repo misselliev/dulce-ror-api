d8cfd398e2a238dcc53955295c53d335884f8f92
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-07 16:38:24 -0700

    Updating talk index controller

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class TalksController < ApplicationController
       before_action :authenticate_api_v1_user!, only: [:index]
       def index
-        user_schedule = current_user.schedule.map{ |item| item.talk.id }
+        user_schedule = @current_api_v1_user.schedule.map { |item| item.talk.id }
         filtered = Talk.where.not(id: user_schedule)
-        # @talks = Talk.all
-        # render json: @talks
         render json: filtered
       end
 
@@ -20,10 +20,10 @@ module Api
       end
 
       private
+
       def talk_params
         params.permit(:title, :description, :location, :date, :time, :speaker_name, :speaker_title, :creator_id)
       end
     end
   end
 end
-
